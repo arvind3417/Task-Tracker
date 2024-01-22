b340ad3182e08492fffd3a6ac7e34ee7218608da
arvind3417 <saivishnuarvind.a20@iiits.in>
2024-01-22 17:20:08 +0530

    user-done

---
+++ user/src/services/user.service.ts
@@ -0,0 +1,86 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { ConfigService } from './config/config.service';
+import { IUser } from '../interfaces/user.interface';
+import { IUserLink } from '../interfaces/user-link.interface';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectModel('User') private readonly userModel: Model<IUser>,
+    @InjectModel('UserLink') private readonly userLinkModel: Model<IUserLink>,
+    private readonly configService: ConfigService,
+  ) {}
+
+  public async searchUser(params: { email: string }): Promise<IUser[]> {
+    return this.userModel.find(params).exec();
+  }
+
+  public async searchUserById(id: string): Promise<IUser> {
+    return this.userModel.findById(id).exec();
+  }
+
+  // public async updateUserById(
+  //   id: string,
+  //   userParams: { is_confirmed: boolean },
+  // ): Promise<IUser> {
+  //   return this.userModel.updateOne({ _id: id }, userParams).exec();
+  // }
+  public async updateUserById(
+    id: string,
+    userParams: { is_confirmed: boolean },
+  ):Promise<IUser| null> {
+    // Use findOneAndUpdate to get the updated document
+    const updatedTask = await this.userModel.findOneAndUpdate(
+      { _id: id },
+      userParams,
+      { new: true } // Return the modified document rather than the original
+    );
+  
+    return updatedTask;
+  }
+
+  public async createUser(user: IUser): Promise<IUser> {
+    const userModel = new this.userModel(user);
+    return await userModel.save();
+  }
+
+  public async createUserLink(id: string): Promise<IUserLink> {
+    const userLinkModel = new this.userLinkModel({
+      user_id: id,
+    });
+    return await userLinkModel.save();
+  }
+
+  public async getUserLink(link: string): Promise<IUserLink[]> {
+    return this.userLinkModel.find({ link, is_used: false }).exec();
+  }
+
+  // public async updateUserLinkById(
+  //   id: string,
+  //   linkParams: { is_used: boolean },
+  // ): Promise<IUserLink> {
+  //   return this.userLinkModel.updateOne({ _id: id }, linkParams);
+  // }
+  public async updateUserLinkById(
+    id: string,
+    linkParams: { is_used: boolean },
+  ):  Promise<IUserLink | null> {
+    // Use findOneAndUpdate to get the updated document
+    const updatedTask = await this.userLinkModel.findOneAndUpdate(
+      { _id: id },
+      linkParams,
+      { new: true } // Return the modified document rather than the original
+    );
+  
+    return updatedTask;
+  }
+
+  public getConfirmationLink(link: string): string {
+    return `${this.configService.get('baseUri')}:${this.configService.get(
+      'gatewayPort',
+    )}/users/confirm/${link}`;
+  }
+}
